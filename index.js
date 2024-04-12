40af34eb27e604915a8409781038bc687168cc3c
Muhammad Mudasir Chandio <159702925+Mudasic1@users.noreply.github.com>
2024-04-12 22:04:24 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let Question = await inquirer.prompt([
+        {
+            name: "Q1",
+            type: "input",
+            message: "What do you like to add in your todos?",
+        },
+        {
+            name: "Q2",
+            type: "confirm",
+            message: "Do you like to add more in your todos?",
+            default: "true",
+        },
+    ]);
+    todos.push(Question.Q1);
+    condition = Question.Q2;
+}
